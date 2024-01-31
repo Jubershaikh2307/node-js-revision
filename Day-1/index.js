06c2713876c00190fb79e673d0254016903f0a20
Juber Shaikh <jubershaikh2307@gmail.com>
2024-01-31 10:03:43 +0530

    finishing the DAy-1

--- Day-1/index.js
+++ Day-1/index.js
@@ -1,34 +1,13 @@
 const http = require("http");
 const fs = require("fs");
+const requestHandler = require("./routes");
 
 http
   .createServer((req, res) => {
-    let { url, method } = req;
-    console.log(url, method);
-    if (url == "/") {
-      console.log("This is server creating"); // this is server creating template
-      res.setHeader("Content-Type", "text/html");
-      res.write(
-        '<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">abcd</button></form></body>'
-      );
-    } else if (url === "/message" && method == "POST") {
-      let data = []; // cannoth get data directly so we have to use the body and res.on
-      req.on("data", (chunk) => {
-        data.push(chunk);
-      });
-      req.on("end", () => {
-        const parsedBody = Buffer.concat(data).toString();
-        const message = parsedBody.split("=")[1];
-        fs.writeFileSync("message.txt", message);
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    } else {
-      console.log("This is server creating");
-      res.setHeader("Content-Type", "text/html");
-      res.write("<h1>Ths is it mail work</h1>");
-    }
-    res.end();
+    requestHandler(req, res);
   })
   .listen(3000);
+
+// Single Thread and Event Loop & Blocking Code
+
+// *Node only use the single thread
---
+++ Day-1/routes.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+
+const requestHandler = (req, res) => {
+  const { url, method } = req;
+  if (url == "/") {
+    console.log("This is server creating"); // this is server creating template
+    res.setHeader("Content-Type", "text/html");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">abcd</button></form></body>'
+    );
+  } else if (url === "/message" && method == "POST") {
+    let data = []; // cannot get data directly so we have to use the body and res.on because we are passing data in through the node not other react library
+    req.on("data", (chunk) => {
+      data.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(data).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (error) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  } else {
+    console.log("This is server creating");
+    res.setHeader("Content-Type", "text/html");
+    res.write("<h1>Ths is it mail work</h1>");
+  }
+  res.end();
+};
+
+module.exports = requestHandler;
